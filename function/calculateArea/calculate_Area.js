8a16c1f9566110b7ccea121c39424b4ed44ffb04
= <=>
2024-02-24 04:53:34 -0500

    debug js code

--- function/calculateArea/calculate_Area.js
+++ function/calculateArea/calculate_Area.js
@@ -1,11 +1,15 @@
 let length;
 let width;
-
-function calculateArea(){
+function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
     width = parseFloat(document.getElementById('width').value);
 
-    let area = length * width
+    let area = length * width;
+    return area
 }
 
-document.getElementById('result').innerText = `The area of the rectangle is ${area}`
+
+function calculateAndDisplayArea() {
+    let area = calculateArea();
+    document.getElementById('result').innerText = `The area of the rectangle is ${area}`;
+}
